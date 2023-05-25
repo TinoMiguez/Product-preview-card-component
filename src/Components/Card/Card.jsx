eb79f62c69efd3fb5fdad23887183ae668ab1cc1
TinoMiguez <cmiguezmartel@gmail.com>
2023-05-25 16:34:20 +0100

    Minor changes on styles. Added cart icon.

--- src/Components/Card/Card.jsx
+++ src/Components/Card/Card.jsx
@@ -7,7 +7,7 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { Container } from "@mui/material";
 import "./Card.css";
-import '../../Fonts/Montserrat-Black.ttf'
+import '../../Fonts/Montserrat-Black.ttf';
 export default function ImgMediaCard() {
   return (
     <>
@@ -53,7 +53,7 @@ export default function ImgMediaCard() {
                 </Typography>
           </CardContent>
           <CardActions>
-              <Button
+              <Button 
                 sx={{
                   width: 300,
                   fontSize: "small",
@@ -61,8 +61,8 @@ export default function ImgMediaCard() {
                   backgroundColor: "darkgreen",
                   color: "white"
                 }}
-              >
-                Add to Cart
+              ><svg width="15" height="16" xmlns="http://www.w3.org/2000/svg"><path d="M14.383 10.388a2.397 2.397 0 0 0-1.518-2.222l1.494-5.593a.8.8 0 0 0-.144-.695.8.8 0 0 0-.631-.28H2.637L2.373.591A.8.8 0 0 0 1.598 0H0v1.598h.983l1.982 7.4a.8.8 0 0 0 .799.59h8.222a.8.8 0 0 1 0 1.599H1.598a.8.8 0 1 0 0 1.598h.943a2.397 2.397 0 1 0 4.507 0h1.885a2.397 2.397 0 1 0 4.331-.376 2.397 2.397 0 0 0 1.12-2.021ZM11.26 7.99H4.395L3.068 3.196h9.477L11.26 7.991Zm-6.465 6.392a.8.8 0 1 1 0-1.598.8.8 0 0 1 0 1.598Zm6.393 0a.8.8 0 1 1 0-1.598.8.8 0 0 1 0 1.598Z" fill="#FFF"/></svg> 
+                <Typography sx={{ fontFamily: "Montserrat", fontSize: "small", marginLeft:1}}>Add to Cart</Typography>
               </Button>
           </CardActions>
         </Card>
